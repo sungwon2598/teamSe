5285da8e74889c01750f3265fe2bbff5fda8fe34
hjg727 <tony9800@naver.com>
2024-11-02 16:12:27 +0900

    KAN-25 feat: Fix: 빈 설정 오류 수정

--- src/main/java/com/seoil/team/config/SecurityConfig.java
+++ src/main/java/com/seoil/team/config/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
                                 "/api-docs/**", "/v3/api-docs/**", "/result", "/",
                                 "/video", "/signal", "/vid", // 추가된 부분
                                 "/error", "/roadmap/**",// 에러 페이지도 허용
-                                "/ws/**"
+                                "/ws/**",//텍스트(STOMP) 채팅 경로
+                                "/signal"//일반 웹소켓 경로
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
